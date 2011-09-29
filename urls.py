1dd2f73bcb6b52e40daf933952892c93f2aef7ce
Fadiga <fadiga@ylm.(none)>
2011-09-29 16:10:59 +0000

    Création des urls

--- urls.py
+++ urls.py
@@ -11,7 +11,18 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r"^$", "ylm_download.views.home", name="home"),
+    url(r"^$", "ylm_download.views.login", name="login"),
+    url(r"^logout$", 'ylm_download.views.logout', name='logout'),
+    url(r"^owner$", "ylm_download.views.owner", name="owner"),
+    url(r"^add_owner$", "ylm_download.views.add_owner", name="add_owner"),
+    url(r"^edit_owner$", "ylm_download.views.edit_owner", name="edit_owner"),
+    url(r"^dashboard$", "ylm_download.views.dashboard", name="dashboard"),
+    url(r"^gestion$", "ylm_download.views.gestion_dl", name="gestion_dl"),
+    url(r"^add_dl$", "ylm_download.views.add_dl", name="add_dl"),
+    url(r"^edit_dl$", "ylm_download.views.edit_dl", name="edit_dl"),
+    url(r"^info_dl$", "ylm_download.views.info_dl", name="info_dl"),
+    url(r"^remove_dl$", "ylm_download.views.dellete_dl", name="dellete"),
+    
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
--- ylm_download/views.py
+++ ylm_download/views.py
@@ -1,10 +1,96 @@
 # Create your views here.
  
 from django.shortcuts import  render_to_response
+from django.core.context_processors import csrf
+
  
- 
-def home(request):
+def login(request):
     """ """
-    c = {}
-    c.update({"user": request.user})
-    return render_to_response("home.html", c)
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "login"})
+    return render_to_response("test.html", ctx)
+
+
+def logout(request):
+    """
+        logout est la views qui permet de se deconnecter
+    """
+
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "logout"})
+    return render_to_response("test.html", ctx)
+
+
+def owner(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "owner"})
+    return render_to_response("test.html", ctx)
+
+
+def add_owner(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "add_owner"})
+    return render_to_response("test.html", ctx)
+
+
+def edit_owner(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "edit_owner"})
+    return render_to_response("test.html", ctx)
+
+
+def dashboard(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "dashboard"})
+    return render_to_response("test.html", ctx)
+
+
+def gestion_dl(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "gestion_dl"})
+    return render_to_response("test.html", ctx)
+
+
+def add_dl(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "add_dl"})
+    return render_to_response("test.html", ctx)
+
+
+def info_dl(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "info_dl"})
+    return render_to_response("test.html", ctx)
+
+
+def dellete_dl(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "dellete_dl"})
+    return render_to_response("test.html", ctx)
+
+
+def edit_dl(request):
+    """ """
+    ctx = {}
+    ctx.update(csrf(request))
+    ctx.update({"page": "edit_dl"})
+    return render_to_response("test.html", ctx)
+
